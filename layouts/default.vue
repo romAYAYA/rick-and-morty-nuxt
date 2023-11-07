94acdb44bb41f3cc050b1578f395568ecae8ec60
Roman Khabayev <supernaturalmlady@gmail.com>
2023-11-08 00:07:38 +0600

    loading added (seems so. got no idea how this magic works in nuxt, wtf)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="flex flex-col min-h-screen bg-[#050404]">
     <Header class="sticky top-0" />
+    <div
+      v-if="loading"
+      class="text-white text-3xl font-bold tracking-tight md:text-4xl xl:text-5xl"
+    >
+      Loading...
+    </div>
     <marquee
       class="text-white text-3xl font-bold tracking-tight md:text-4xl xl:text-5xl"
       >Yeah, I'm DESIGNER</marquee
@@ -12,4 +18,15 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const nuxtApp = useNuxtApp()
+const loading = ref(false)
+
+nuxtApp.hook('page:start', () => {
+  loading.value = true
+})
+
+nuxtApp.hook('page:finish', () => {
+  loading.value = false
+})
+</script>
